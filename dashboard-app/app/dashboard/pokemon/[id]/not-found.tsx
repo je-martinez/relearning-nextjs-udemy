7f2b10dd135d76f2cec02511ceaf7257a93972d6
Jose E. Martinez <josemartinezchevez@gmail.com>
2025-11-20 23:12:15 -0600

    fix: update NotFound page navigation link to redirect to Pokemons page and change button text for clarity

--- dashboard-app/app/dashboard/pokemon/[id]/not-found.tsx
+++ dashboard-app/app/dashboard/pokemon/[id]/not-found.tsx
@@ -11,13 +11,13 @@ export default function NotFound() {
       </div>
       <button className="mt-5">
         <Link
-          href="/dashboard/main"
+          href="/dashboard/pokemons"
           className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring"
         >
           <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D]"></span>
 
           <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
-            Go Home
+            Go to Pokemons
           </span>
         </Link>
       </button>
