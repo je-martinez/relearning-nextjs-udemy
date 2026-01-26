6cfda664e3b9fb7faa208c6b83ba665e797197d9
Jose E. Martinez <josemartinezchevez@gmail.com>
2026-01-25 20:34:16 -0600

    feat: enhance todo actions with optimistic UI updates and delay in toggle functionality

--- admin-todo/src/todos/actions/todo-actions.ts
+++ admin-todo/src/todos/actions/todo-actions.ts
@@ -1,55 +1,68 @@
-'use server';
+"use server";
 
 import { Todo } from "@/generated/prisma/client";
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
-export const toogleTodoAction = async (id: string, completed: boolean):Promise<Todo> => {
-    const todo = await prisma.todo.findUnique({
-        where: { id },
-    });
+const delay = (seconds: number) =>
+  new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 
-    if (!todo) {
-        throw new Error(`Todo with id ${id} not found`);
-    }
+export const toogleTodoAction = async (
+  id: string,
+  completed: boolean,
+): Promise<Todo> => {
+  await delay(5);
 
-    const updatedTodo = await prisma.todo.update({
-        where: { id },
-        data: { completed },
+  const todo = await prisma.todo.findUnique({
+    where: { id },
+  });
+
+  if (!todo) {
+    throw new Error(`Todo with id ${id} not found`);
+  }
+
+  const updatedTodo = await prisma.todo.update({
+    where: { id },
+    data: { completed },
+  });
+
+  revalidatePath("/dashboard/server-todos");
+  return updatedTodo;
+};
+
+export const addTodoAction = async (
+  title: string,
+  description: string,
+): Promise<Todo | { message: string }> => {
+  try {
+    const todo = await prisma.todo.create({
+      data: { title, description, completed: false },
     });
-    
     revalidatePath("/dashboard/server-todos");
-    return updatedTodo;
+    return todo;
+  } catch (error) {
+    console.error(error);
+    return {
+      message: "Failed to add todo",
+    };
+  }
 };
 
-export const addTodoAction = async (title: string, description: string):Promise<Todo | { message: string }> => {
-    try {
-        const todo = await prisma.todo.create({
-            data: { title, description, completed: false },
-        });
-        revalidatePath("/dashboard/server-todos");
-        return todo;
-    } catch (error) {
-        console.error(error);
-        return {
-            message: "Failed to add todo",
-        };
-    }
-};
-
-export const deleteCompletedTodosAction = async ():Promise<{ message: string }> => {
-    try {
-        await prisma.todo.deleteMany({
-            where: { completed: true },
-        });
-        revalidatePath("/dashboard/server-todos");
-        return {
-            message: "Completed todos deleted",
-        };
-    } catch (error) {
-        console.error(error);
-        return {
-            message: "Failed to delete completed todos",
-        };
-    }
+export const deleteCompletedTodosAction = async (): Promise<{
+  message: string;
+}> => {
+  try {
+    await prisma.todo.deleteMany({
+      where: { completed: true },
+    });
+    revalidatePath("/dashboard/server-todos");
+    return {
+      message: "Completed todos deleted",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      message: "Failed to delete completed todos",
+    };
+  }
 };
--- admin-todo/src/todos/components/TodoItem.tsx
+++ admin-todo/src/todos/components/TodoItem.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { Todo } from "@/generated/prisma/client";
 import { CiSquareCheck } from "react-icons/ci";
 import styles from "./TodoItem.module.css";
+import { startTransition, useMemo, useOptimistic } from "react";
 
 interface TodoItemProps {
   todo: Todo;
@@ -8,20 +11,45 @@ interface TodoItemProps {
 }
 
 export const TodoItem = ({ todo, onToggle }: TodoItemProps) => {
-  const todoClass = todo.completed ? styles.todoDone : styles.todoPending;
+  const [optimisticTodo, toggleTodoOptimistic] = useOptimistic(
+    todo,
+    (state, newCompleted: boolean) => {
+      return {
+        ...state,
+        completed: newCompleted,
+      };
+    },
+  );
+
+  const onToogleTodo = async () => {
+    try {
+      startTransition(() => toggleTodoOptimistic(!optimisticTodo.completed));
+      await onToggle(optimisticTodo.id, !optimisticTodo.completed);
+    } catch (error) {
+      console.error("Error toggling todo", error);
+      startTransition(() => toggleTodoOptimistic(!optimisticTodo.completed));
+    }
+  };
+
+  const todoClass = useMemo(
+    () => (optimisticTodo.completed ? styles.todoDone : styles.todoPending),
+    [optimisticTodo.completed],
+  );
 
   return (
     <div className={todoClass}>
       <div className="flex flex-col sm:flex-row items-center gap-2">
-        <button
-          className="w-6 h-6 cursor-pointer"
-          onClick={() => onToggle(todo.id, !todo.completed)}
-        >
-          <CiSquareCheck size={20} color={todo.completed ? "green" : "red"} />
+        <button className="w-6 h-6 cursor-pointer" onClick={onToogleTodo}>
+          <CiSquareCheck
+            size={20}
+            color={optimisticTodo.completed ? "green" : "red"}
+          />
         </button>
         <div className="px-4 flex flex-col gap-2">
-          <h2 className="text-lg font-bold text-black">{todo.title}</h2>
-          <p className="text-sm text-gray-500">{todo.description}</p>
+          <h2 className="text-lg font-bold text-black">
+            {optimisticTodo.title}
+          </h2>
+          <p className="text-sm text-gray-500">{optimisticTodo.description}</p>
         </div>
       </div>
     </div>
