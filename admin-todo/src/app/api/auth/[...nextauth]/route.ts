fc7704440f0d0ca6f244110d56c9389429913157
Jose E. Martinez <josemartinezchevez@gmail.com>
2026-02-07 19:57:01 -0600

    feat: update authentication setup by replacing NextAuth configuration and removing deprecated auth.ts file

--- admin-todo/src/app/api/auth/[...nextauth]/route.ts
+++ admin-todo/src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,14 @@
-import { handlers } from "@/auth";
-export const { GET, POST } = handlers;
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID ?? "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
+    }),
+  ],
+};
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- admin-todo/src/app/dashboard/page.tsx
+++ admin-todo/src/app/dashboard/page.tsx
@@ -1,11 +1,16 @@
 import { ArrowRevenue } from "@/components";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/auth-options";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+
   return (
     <>
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         <div className="md:col-span-2 lg:col-span-1">
           <div className="max-w-full max-h-96 p-8 space-y-6 rounded-xl border border-gray-200 bg-white">
+            <WelcomeLoggedInUser />
             <div className="flex flex-col gap-2 w-full h-full">
               <h5 className="text-xl text-gray-600 text-center">
                 Global Activities
@@ -24,3 +29,12 @@ export default function DashboardPage() {
     </>
   );
 }
+
+export const WelcomeLoggedInUser = async () => {
+  const session = await getServerSession(authOptions);
+  return (
+    <div>
+      <h1>Welcome {session?.user?.name}</h1>
+    </div>
+  );
+};
