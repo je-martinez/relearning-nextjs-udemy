3105cd76c5fe7f0f55a6eb23ee7246581b672cbe
Jose E. Martinez <josemartinezchevez@gmail.com>
2026-03-24 22:16:47 -0600

    feat: enhance ProfilePage layout by replacing user information display with a structured table and adding a JSON representation of the session data

--- admin-todo/src/app/dashboard/profile/page.tsx
+++ admin-todo/src/app/dashboard/profile/page.tsx
@@ -6,13 +6,33 @@ export default function ProfilePage() {
   const { data: session } = useSession();
 
   return (
-    <div className="max-w-full max-h-96 p-8 space-y-6 rounded-xl border border-gray-200 bg-white">
-      <h1 className="text-2xl font-bold text-black">
-        Profile Page (Client Side)
-      </h1>
-      <p className="text-gray-500">{session?.user?.name}</p>
-      <p className="text-gray-500">{session?.user?.email}</p>
-      <p className="text-gray-500">{session?.user?.image}</p>
-    </div>
+    <>
+      <div className="max-w-full max-h-96 p-8 space-y-6 rounded-xl border border-gray-200 bg-white">
+        <h1 className="text-2xl font-bold text-black">
+          Profile Page (Client Side)
+        </h1>
+        <table className="w-full">
+          <tbody>
+            <tr>
+              <td className="text-gray-500">Session User Name:</td>
+              <td className="text-gray-500">{session?.user?.name}</td>
+            </tr>
+            <tr>
+              <td className="text-gray-500">Session User Email:</td>
+              <td className="text-gray-500">{session?.user?.email}</td>
+            </tr>
+            <tr>
+              <td className="text-gray-500">Session User Image:</td>
+              <td className="text-gray-500">{session?.user?.image}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="max-w-full mt-4 min-h-72 p-8 space-y-6 rounded-xl border border-gray-200 bg-white">
+        <pre className="text-gray-500 max-h-96 overflow-y-auto">
+          <code>{JSON.stringify(session, null, 4)}</code>
+        </pre>
+      </div>
+    </>
   );
 }
