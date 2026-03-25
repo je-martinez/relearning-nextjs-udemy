cee24b93dc53c687989f221f6de28bd14bf04bfd
Jose E. Martinez <josemartinezchevez@gmail.com>
2026-03-24 22:35:39 -0600

    feat: update user model to include roles and isActive fields, enhance NextAuth configuration with JWT session strategy and user role management

--- admin-todo/src/app/api/auth/[...nextauth]/route.ts
+++ admin-todo/src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import prisma from "@/lib/prisma";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -17,6 +17,29 @@ export const authOptions = {
     }),
   ],
   adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async signIn() {
+      return true;
+    },
+    async jwt({ token }: { token: any }) {
+      const dbUser = await prisma.user.findUnique({
+        where: {
+          email: token.email ?? "unknown",
+        },
+      });
+      token.roles = dbUser?.roles ?? ["unknown"];
+      token.id = dbUser?.id ?? "unknown";
+      return token;
+    },
+    async session({ session, token }: { session: any; token: any }) {
+      session.user.id = token.id;
+      session.user.roles = token.roles;
+      return session;
+    },
+  },
 };
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions as AuthOptions);
 export { handler as GET, handler as POST };
