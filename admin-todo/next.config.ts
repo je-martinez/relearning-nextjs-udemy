16ff6d0adc668e59cd05233284e6999bfe8fbc52
Jose E. Martinez <josemartinezchevez@gmail.com>
2026-03-24 21:38:06 -0600

    feat: extend authentication setup by adding Google OAuth credentials and updating NextAuth configuration

--- admin-todo/next.config.ts
+++ admin-todo/next.config.ts
@@ -35,6 +35,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      }
     ],
   },
 };
